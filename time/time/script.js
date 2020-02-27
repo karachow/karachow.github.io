ff080e3c59415dc490422bf9c8b487588eff4efa
karachow <55113073+karachow@users.noreply.github.com>
2020-02-27 00:30:39 -0500

    Add files via upload

---
+++ time/time/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  
+  setTimeout(function() {
+	$(".one").addClass("alarm");
+	$(".two").addClass("alarm");
+	$(".three").addClass("alarm");
+		$(".wat").addClass("alarm");
+  }, 5000);
+});
