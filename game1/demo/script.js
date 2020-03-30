2d2a44a896f92d41263d119496bfec070a871184
karachow <55113073+karachow@users.noreply.github.com>
2020-03-30 14:25:54 -0400

    Add files via upload

--- game1/demo/script.js
+++ game1/demo/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 1200, npcCollisionRadius: 100,  jumpHeight: 300, actionDuration:700, heroSpeed: 200});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 2700, npcCollisionRadius: 100,  jumpHeight: 300, actionDuration:700, heroSpeed: 200, removeNPCAfterInteractedDelay: 5000});
 });
